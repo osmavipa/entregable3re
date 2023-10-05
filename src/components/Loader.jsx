e21e57f84e7c7d663314571d27f45eedf8d772d0
MauroVillaPaez <140631744+osmavipa@users.noreply.github.co>
2023-10-04 20:52:49 -0500

    subiendo rep

---
+++ src/components/Loader.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <>
+        <h2>Loading</h2>
+            <div className="spinner">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </>
+    )
+}
+
+export default Loader
